4a6c274753fa4e127b4036f19c82cf36ea1ea132
dpwrussell <douglas_russell@hms.harvard.edu>
2016-03-08 14:18:47 +0000

    Display Cube based on actual Cells and Datasets and enabled filtering

--- cancerbrowser/app/Cube.jsx
+++ cancerbrowser/app/Cube.jsx
@@ -3,15 +3,37 @@ import React from 'react';
 export default class Cube extends React.Component {
 
   render() {
-    const { datasets, cells, cellsInDatasets } = this.props;
 
-    let rows = Object.keys(datasets).map(datasetId => {
+    const { datasets, resultingDatasetIds, cells, resultingCellIds, cellsInDatasets } = this.props;
+
+    let columns = resultingCellIds.map(cellId => {
+      let cell = cells[cellId];
+
       return (
-        <tr key={datasetId}>
-          <th scope="row">{ datasets[datasetId].name }</th>
-          <td className="success"></td>
-          <td className="success"></td>
-          <td></td>
+        <th key={ cellId }>{ cell.name }</th>
+      )
+    });
+
+    let rows = resultingDatasetIds.map(datasetId => {
+      let dataset = datasets[datasetId];
+
+      let columns = resultingCellIds.map(cellId => {
+
+        if (cellsInDatasets[datasetId] && cellsInDatasets[datasetId].indexOf(cellId) != -1) {
+          return (
+            <td key={ cellId } className="success"></td>
+          )
+        }
+
+        return (
+          <td key={ cellId }></td>
+        )
+      });
+
+      return (
+        <tr key={ datasetId }>
+          <th scope="row">{ dataset.name }</th>
+          { columns }
         </tr>
       );
     });
@@ -25,9 +47,7 @@ export default class Cube extends React.Component {
             <thead>
               <tr>
                 <th>Datasets</th>
-                <th>X</th>
-                <th>Y</th>
-                <th>Z</th>
+                { columns }
               </tr>
             </thead>
             <tbody>
