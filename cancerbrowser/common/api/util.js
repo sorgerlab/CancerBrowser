e049c6a47085d7c8793b6dd69bd7a58f2877a4dc
Jim Vallandingham <vlandham@gmail.com>
2016-05-24 09:19:25 -0700

    Pull out categorical matching to separate function.
    Rename to follow along with filter grouping concept

--- cancerbrowser/common/api/util.js
+++ cancerbrowser/common/api/util.js
@@ -1,40 +1,30 @@
 import _ from 'lodash';
 
 /**
- * Filter a given row based dataset based on the filters provided.
+ * Filter a given row based dataset based on the categorical filters provided.
  *
  * dataset is an array of objects with consistent attributes:
- * [{id:'MMM', name:'', collection:['a','b']}]
+ * [{id:'MMM', name:'', collection:['All','Big6']}]
  *
- * filters is expected to be an array of filterGroups.
- * Each filterGroup is an object with an 'id' string and an
+ * filterGroup is expected to be an array of filters in a filterGroup.
+ * Each filter is an object with an 'id' string and an
  * array of strings in 'values'.
- * Example: {id:'collections', values:['Big6']}
+ * Example: {id:'collection', values:['Big6']}
  *
- * The filters work as a OR-ing within a filterGroup and
- * AND-ing between filterGroups.
+ * The filters work as a OR-ing within a filter and
+ * AND-ing between filters.
  *
  * @param {Array} dataset Array of data objects
- * @param {Array} filters Array of filterGroup objects
+ * @param {Array} filterGroup Array of filter objects
  *
  */
-export function filterData(dataset, filters) {
+export function filterData(dataset, filterGroup) {
   var filteredDataset;
 
-  if(filters && filters.length > 0) {
+  if(filterGroup && filterGroup.length > 0) {
     filteredDataset = _.filter(dataset, function(row) {
 
-      var matches = _.map(filters, function(filter) {
-        var matched = false;
-        var possibleValues = _.flatten([filter.values]);
-        // see if any of the data values are in the filter values
-        _.flatten([row[filter.id]]).forEach(function(value) {
-          if(_.includes(possibleValues, value)) {
-            matched = true;
-          }
-        });
-        return matched;
-      });
+      var matches = getCategoricalMatches(row, filterGroup);
 
       // now matches is an array of true/false values.
       // if there is a false value in matches, then then
@@ -46,4 +36,31 @@ export function filterData(dataset, filters) {
     filteredDataset = dataset;
   }
   return filteredDataset;
+}
+
+/*
+ * Returns an array with one value for each value in the filterGroup.
+ *
+ * For each filter in the filter group, the array contains a true if
+ * an attribute in the data row matches one of the allowed values for the
+ * filter, and false otherwise.
+ *
+ * @param {Array} row Object row of the data
+ * @param {Array} filterGroup Array of filter objects
+ *
+ */
+function getCategoricalMatches(row, filterGroup) {
+  var matches = _.map(filterGroup, function(filter) {
+    var matched = false;
+    var possibleValues = _.flatten([filter.values]);
+    // see if any of the data values are in the filter values
+    _.flatten([row[filter.id]]).forEach(function(value) {
+      if(_.includes(possibleValues, value)) {
+        matched = true;
+      }
+    });
+    return matched;
+  });
+
+  return matches;
 }
