67d7e830baa460e5ee3c065ab6b86d3dae457933
Peter Beshai <peter.beshai@gmail.com>
2016-05-24 12:17:43 -0400

    Remove unused cellLines prop from sample data

--- cancerbrowser/common/containers/CellLineBrowserPage/index.jsx
+++ cancerbrowser/common/containers/CellLineBrowserPage/index.jsx
@@ -67,74 +67,74 @@ const cellLineFilters = [
     label: 'Collection',
     type: 'multi-select',
     values: [
-      { value: 'big6', label: 'Big 6', cellLines: [1,2,3,4,5,6] },
-      { value: 'icbp43', label: 'ICBP43', cellLines: [1,2,3,4,5,6,7,8,9,10] }
+      { value: 'big6', label: 'Big 6' },
+      { value: 'icbp43', label: 'ICBP43' }
     ]
   }, {
     id: 'receptorStatus',
     label: 'Receptor Status',
     type: 'multi-select',
     values: [
-      { value: 'nm', label: 'NM', cellLines: [1,2,3,4,5,6] },
-      { value: 'her2amp', label: 'HER2amp', cellLines: [1,2,3,4,5,6] },
-      { value: 'tnbc', label: 'TNBC', cellLines: [1,2,3,4,5,6] },
-      { value: 'hrplus', label: 'HR+', cellLines: [1,2,3,4,5,6] }
+      { value: 'nm', label: 'NM' },
+      { value: 'her2amp', label: 'HER2amp' },
+      { value: 'tnbc', label: 'TNBC' },
+      { value: 'hrplus', label: 'HR+' }
     ]
   }, {
     id: 'molecularSubtype',
     label: 'Molecular Subtype',
     type: 'multi-select',
     values: [
-      { value: 'basal', label: 'Basal', cellLines: [1,2,3,4,5,6] },
-      { value: 'basalA', label: 'Basal A', cellLines: [1,2,3,4,5,6] },
-      { value: 'basalB', label: 'Basal B', cellLines: [1,2,3,4,5,6] },
-      { value: 'luminal', label: 'Luminal', cellLines: [1,2,3,4,5,6] },
-      { value: 'claudin', label: 'Low Claudin Status', cellLines: [1,2,3,4,5,6] }
+      { value: 'basal', label: 'Basal' },
+      { value: 'basalA', label: 'Basal A' },
+      { value: 'basalB', label: 'Basal B' },
+      { value: 'luminal', label: 'Luminal' },
+      { value: 'claudin', label: 'Low Claudin Status' }
     ]
   }, {
     id: 'mutation',
     label: 'Mutation Status',
     type: 'multi-select',
     values: [
-      { value: 'brca1wt', label: 'BRCA1 WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'brca1mut', label: 'BRCA1 MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'brca2wt', label: 'BRCA2 WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'brca2mut', label: 'BRCA2 MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'cdh1wt', label: 'CDH1 WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'cdh1mut', label: 'CDH1 MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'map3k1wt', label: 'MAP3K1 WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'map3k1mut', label: 'MAP3K1 MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'mll3wt', label: 'MLL3 WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'mll3mut', label: 'MLL3 MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'pik3cawt', label: 'PIK3CA WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'pik3camut', label: 'PIK3CA MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'ptenwt', label: 'PTEN WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'ptenmut', label: 'PTEN MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'tp53wt', label: 'TP53 WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'tp53mut', label: 'TP53 MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'gata3wt', label: 'GATA3 WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'gata3mut', label: 'GATA3 MUT', cellLines: [1,2,3,4,5,6] },
-      { value: 'map2k4wt', label: 'MAP2K4 WT', cellLines: [1,2,3,4,5,6] },
-      { value: 'map2k4mut', label: 'MAP2K4 MUT', cellLines: [1,2,3,4,5,6] }
+      { value: 'brca1wt', label: 'BRCA1 WT' },
+      { value: 'brca1mut', label: 'BRCA1 MUT' },
+      { value: 'brca2wt', label: 'BRCA2 WT' },
+      { value: 'brca2mut', label: 'BRCA2 MUT' },
+      { value: 'cdh1wt', label: 'CDH1 WT' },
+      { value: 'cdh1mut', label: 'CDH1 MUT' },
+      { value: 'map3k1wt', label: 'MAP3K1 WT' },
+      { value: 'map3k1mut', label: 'MAP3K1 MUT' },
+      { value: 'mll3wt', label: 'MLL3 WT' },
+      { value: 'mll3mut', label: 'MLL3 MUT' },
+      { value: 'pik3cawt', label: 'PIK3CA WT' },
+      { value: 'pik3camut', label: 'PIK3CA MUT' },
+      { value: 'ptenwt', label: 'PTEN WT' },
+      { value: 'ptenmut', label: 'PTEN MUT' },
+      { value: 'tp53wt', label: 'TP53 WT' },
+      { value: 'tp53mut', label: 'TP53 MUT' },
+      { value: 'gata3wt', label: 'GATA3 WT' },
+      { value: 'gata3mut', label: 'GATA3 MUT' },
+      { value: 'map2k4wt', label: 'MAP2K4 WT' },
+      { value: 'map2k4mut', label: 'MAP2K4 MUT' }
     ]
   }, {
     id: 'malignancy',
     label: 'Malignancy Status',
     type: 'multi-select',
     values: [
-      { value: 'malignant', label: 'Malignant', cellLines: [1,2,3,4,5,6] },
-      { value: 'nonmalignant', label: 'Non-malignant', cellLines: [1,2,3,4,5,6] }
+      { value: 'malignant', label: 'Malignant' },
+      { value: 'nonmalignant', label: 'Non-malignant' }
     ]
   }, {
     id: 'dataset',
     label: 'Dataset',
     type: 'multi-select',
     values: [
-      { value: 'dataset1', label: 'Basal Receptor (RTK) Profile', cellLines: [1,2,3,4,5,6] },
-      { value: 'dataset2', label: 'Growth Factor-Induced pAKT/pERK Response', cellLines: [1,2,3,4,5,6] },
-      { value: 'dataset3', label: 'Basal Total Protein Mass Spectrometry', cellLines: [1,2,3,4,5,6] },
-      { value: 'dataset4', label: 'Basal Phosphoprotein Mass Spectrometry', cellLines: [1,2,3,4,5,6] },
-      { value: 'dataset5', label: 'Drug Dose-Response Growth Rate Profiling', cellLines: [1,2,3,4,5,6] }
+      { value: 'dataset1', label: 'Basal Receptor (RTK) Profile' },
+      { value: 'dataset2', label: 'Growth Factor-Induced pAKT/pERK Response' },
+      { value: 'dataset3', label: 'Basal Total Protein Mass Spectrometry' },
+      { value: 'dataset4', label: 'Basal Phosphoprotein Mass Spectrometry' },
+      { value: 'dataset5', label: 'Drug Dose-Response Growth Rate Profiling' }
     ],
     options: {
       props: {
