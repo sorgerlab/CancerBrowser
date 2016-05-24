263861e5bf1c0c51f8d978b2dbd5c0cc59e1c5fb
Peter Beshai <peter.beshai@gmail.com>
2016-05-24 15:38:22 -0400

    Add in jsdoc comments to utils #44

--- cancerbrowser/common/utils/boundCallback.js
+++ cancerbrowser/common/utils/boundCallback.js
@@ -1,6 +1,12 @@
 /**
  * Function enables memoization of callbacks bound with arguments to
  * enable PureRenderMixin to work effectively.
+ *
+ * @param {Object} context What `this` to bind
+ * @param {Object} cache An object for memoizing the bindings
+ * @param {Function} callback The function to bind
+ * @param {...} ...keys All remaining parameters are passed as arguments to `callback`
+ * @return {Function} The bound callback
  */
 function boundCallback(context, cache, callback, ...keys) {
   const strKey = JSON.stringify(keys);
