48f9c8bb5c2581921045ec4727edc1316faeff9c
Jim Vallandingham <vlandham@gmail.com>
2016-05-24 09:53:38 -0700

    Modified getCellLines to take in multiple filterGroups.
    Added tests for multiple filter groups and 
    multiple filters.

--- cancerbrowser/common/api/cell_line.js
+++ cancerbrowser/common/api/cell_line.js
@@ -1,9 +1,15 @@
 
 import { filterData } from './util';
 
+import _ from 'lodash';
+
 //TODO async load cell line data.
 import cellLines from  './data/cell_lines.json';
 
-export function getCellLines(filters) {
-  return filterData(cellLines, filters);
+export function getCellLines(filterGroups) {
+  var filteredCellLines = _.clone(cellLines);
+  filterGroups.forEach(function(filterGroup) {
+    filteredCellLines = filterData(filteredCellLines, filterGroup);
+  });
+  return filteredCellLines;
 }
