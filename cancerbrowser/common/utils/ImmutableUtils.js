899f831fb29c6b5fe71d2f6a24ad85f62a5731c6
Peter Beshai <peter.beshai@gmail.com>
2016-05-24 09:55:20 -0400

    Switch to spread operator for clarity

--- cancerbrowser/common/utils/ImmutableUtils.js
+++ cancerbrowser/common/utils/ImmutableUtils.js
@@ -3,12 +3,12 @@
  * except array[index] === value
  */
 export function arraySet(array, index, value) {
-  return array.slice(0, index).concat([value]).concat(array.slice(index + 1));
+  return [...array.slice(0, index), value, ...array.slice(index + 1)];
 }
 
 /**
  * Creates a new array with array[index] dropped
  */
 export function arrayRemove(array, index) {
-  return array.slice(0, index).concat(array.slice(index + 1));
+  return [...array.slice(0, index), ...array.slice(index + 1)];
 }
