1ca030540fb71480288503eb7e9fae78fc8bec72
Peter Beshai <peter.beshai@gmail.com>
2016-05-31 13:31:57 -0400

    Modify drug parser to have an explicit format #7

--- cancerbrowser/tools/parse_drugs.js
+++ cancerbrowser/tools/parse_drugs.js
@@ -1,15 +1,38 @@
 #!/usr/bin/env node
+/*
+ Sample output:
+  [{
+    hmsLincsId: '10001-101',
+    name: { value: 'someName', label: 'Some Name' },
+    nominalTarget: { value: 'someTarget', label: 'Some Target' },
+    parentTargets: ['someOtherTargetValue', 'someOtherParentTargetValue', ...],
+    class: { value: 'phase1', label: 'Phase 1 '},
+    synonyms: ['someName', 'someOtherName', ...],
+    searchIndexOnlyNames: ['hiddenName', 'hiddenOtherName', ...],
+  }, ...]
+*/
 
 var fs = require('fs');
 var d3 = require('d3');
 var _  = require('lodash');
-
 require('./utils');
 
-function toCategories(string, delim) {
-  return _.split(string, delim).map((s) => {
-    return {label: _.trim(s), value:_.trim(s).replace(' ', '').toLowerCase()};
-  });
+
+function normalize(str) {
+  return _.trim(str).replace(/[\s-]/g, '').toLowerCase();
+}
+
+function labelValue(str) {
+  const trimmed = _.trim(str);
+
+  if (trimmed.length === 0) {
+    return null;
+  }
+
+  return {
+    label: trimmed,
+    value: normalize(str)
+  };
 }
 
 var filename = process.argv[2];
@@ -20,29 +43,18 @@ var outputFilename = './drugs.json';
 fs.readFile(filename, 'utf8', function(error, data) {
   data = d3.csv.parse(data);
 
-  data.forEach(function(d) {
-
-    Object.keys(d).forEach(function(k) {
-
-      var newKey = k.toLowerCase();
-      newKey = newKey.replace(/\s/g,'');
-      newKey = newKey.replace(/-/g,'');
-      d.renameProperty(k, newKey);
-
-      var value = d[newKey];
-
-      if(value) {
-        var newValue = value.toLowerCase();
-
-        d[newKey] = {value: newValue, label:value};
-      } else {
-        d[newKey] = {value: '', label: ''};
-      }
-    });
-
-    d.searchindexonlynames = toCategories(d.searchindexonlynames.label, ';');
-    d.id = d.name.value;
+  const transformed = data.map(function(d) {
+    return {
+      id: d['HMS LINCS ID'],
+      hmsLincsId: d['HMS LINCS ID'],
+      name: labelValue(d['Name']),
+      nominalTarget: labelValue(d['Nominal target / Pathway']),
+      parentTargets: [],
+      class: labelValue(d['Class']),
+      synonyms: _.compact(_.split(d['Synonyms'], ';')),
+      searchIndexOnlyNames: _.compact(_.split(d['Search-index-only names'], ';'))
+    };
   });
 
-  fs.writeFileSync(outputFilename, JSON.stringify(data, null, 2));
+  fs.writeFileSync(outputFilename, JSON.stringify(transformed, null, 2));
 });
