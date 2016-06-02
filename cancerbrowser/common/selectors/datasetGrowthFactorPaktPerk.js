748bf04453500dc5d9a3eca1e82270b27c6c25b1
Peter Beshai <peter.beshai@gmail.com>
2016-06-02 18:27:25 -0400

    Remove unused code

--- cancerbrowser/common/selectors/datasetGrowthFactorPaktPerk.js
+++ cancerbrowser/common/selectors/datasetGrowthFactorPaktPerk.js
@@ -21,53 +21,6 @@ function filterDataByCellLines(data, cellLines) {
   return data.filter(d => cellLines.find(cellLine => cellLine.id === d.cell_line.id));
 }
 
-
-/**
- * Transform Receptor data to be oriented by receptor instead of
- * cell line data.
- * @param {Array} dataset Dataset expected to be processed by
- * transformReceptorData already.
- *
- */
-function convertToByReceptor(dataset) {
-  if (!dataset) {
-    return dataset;
-  }
-
-  // Store each receptor found separately.
-  let receptors = {};
-
-  dataset.forEach(function(cellLine) {
-
-    // iterate over receptor data
-    cellLine.measurements.forEach(function(m) {
-      // pull out receptor if it isn't in the receptors hash already.
-      receptors[m.id]  = receptors[m.id] || {id:m.id, label:m.receptor, measurements: []};
-      // add a new cell line based measurement
-      let newMeasurement = {id: cellLine.id, label: cellLine.label, value:m.value, threshold:m.threshold};
-      receptors[m.id].measurements.push(newMeasurement);
-    });
-  });
-
-  return _.values(receptors);
-}
-
-
-/** Filters data organized by receptor based on provided cell lines */
-function filterReceptorDataByCellLine(data, cellLines) {
-  if (!data) {
-    return data;
-  }
-
-  return data.map(d => {
-    const filteredMeasurements = d.measurements.filter(m => {
-      return cellLines.find(cellLine => cellLine.id === m.id);
-    });
-
-    return Object.assign({}, d, { measurements: filteredMeasurements });
-  });
-}
-
 /////////////////////
 // Selectors
 /////////////////////
@@ -77,7 +30,7 @@ export const getViewData = createSelector(
   [ getDataset(datasetId), getViewBy(datasetKey) ],
   (dataset, viewBy) => {
     if (viewBy === 'growthFactor') {
-      return dataset; //convertToByReceptor(dataset);
+      return dataset; // TODO
     } else {
       return dataset;
     }
@@ -90,8 +43,7 @@ export const getFilteredViewData = createSelector(
   [ getViewData, getViewBy(datasetKey), getFilteredCellLines ],
   (viewData, viewBy, filteredCellLines) => {
     if (viewBy === 'growthFactor') {
-      return viewData;
-      // return filterReceptorDataByCellLine(viewData, filteredCellLines);
+      return viewData; // TODO
     } else {
       return filterDataByCellLines(viewData, filteredCellLines);
     }
