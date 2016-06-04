a3f32d0558b0fa7193ed2bebfd65335f148b5bf5
Peter Beshai <peter.beshai@gmail.com>
2016-06-03 20:54:06 -0400

    Support strokes on thresholded bars

--- cancerbrowser/common/components/WaterfallPlot/waterfall.js
+++ cancerbrowser/common/components/WaterfallPlot/waterfall.js
@@ -140,6 +140,8 @@ class Waterfall {
         .on('mouseout', this.onMouseout);
     }
 
+    // minus 2 to make room for the stroke
+    const barHeight = scales.y.rangeBand() - 2;
 
     // for easier mouseovering
     const hoverBars = this.g
@@ -156,7 +158,7 @@ class Waterfall {
       .attr('x', 0)
       .attr('y', (d) => scales.y(d.id))
       .attr('width', this.width)
-      .attr('height', scales.y.rangeBand())
+      .attr('height', barHeight)
       .style('fill', '#fff')
       .on('mouseover', this.onMouseover)
       .on('mouseout', this.onMouseout);
@@ -175,7 +177,7 @@ class Waterfall {
       .attr('x', 0)
       .attr('y', (d) => scales.y(d.id))
       .attr('width', (d) => d.disabled ? 0 : scales.x(d.value))
-      .attr('height', scales.y.rangeBand() - 2)
+      .attr('height', barHeight)
       .style('fill', (d) => scales.color(d))
       .style('stroke', (d) => d3.rgb(scales.color(d)).darker(1).toString())
       .classed('highlight', (d)  => d.id === highlightId)
@@ -198,7 +200,8 @@ class Waterfall {
         .attr('x', 0)
         .attr('y', (d) => scales.y(d.id))
         .attr('width', (d) => d.disabled ? DISABLED_BAR_SIZE : scales.x(d.threshold))
-        .attr('height', scales.y.rangeBand())
+        .attr('height', barHeight)
+        .style('stroke', (d) => d3.rgb(scales.color(d)).darker(1).toString())
         .classed('highlight', (d)  => d.id === highlightId)
         .on('mouseover', this.onMouseover)
         .on('mouseout', this.onMouseout);
