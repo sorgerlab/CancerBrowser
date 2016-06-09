8717598f18ed10054d34a91af61a905f71d2f6d7
Jim Vallandingham <vlandham@gmail.com>
2016-06-09 09:52:58 -0700

    remove cell line test as filtering is done in selectors

--- cancerbrowser/test/api/filter_data.test.js
+++ cancerbrowser/test/api/filter_data.test.js
@@ -10,17 +10,17 @@ import {
 describe('API Util', function() {
   it('filters data with string attributes ', function() {
 
-    var data = [
+    const data = [
       {id:'first', attribute: {value: 'correct'}},
       {id:'second', attribute: {value: 'incorrect'}},
       {id:'third', attribute: {value: 'valid'}}
     ];
 
-    var filters = [
+    const filters = [
       {id:'attribute', values:['correct', 'valid', 'yes']}
     ];
 
-    var results = filterData(data, filters);
+    const results = filterData(data, filters);
     assert.equal(results.length, 2);
     assert.equal(results[0].id, 'first');
     assert.equal(results[1].id, 'third');
@@ -28,17 +28,17 @@ describe('API Util', function() {
 
   it('filters data with array attributes ', function() {
 
-    var data = [
+    const data = [
       {id:'first', attribute:[{value:'incorrect'}]},
       {id:'second', attribute:[{value:'correct'}, {value:'another'}]},
       {id:'third', attribute:{value:'valid'}}
     ];
 
-    var filters = [
+    const filters = [
       {id:'attribute', values:['correct', 'valid']}
     ];
 
-    var results = filterData(data, filters);
+    const results = filterData(data, filters);
     assert.equal(results.length, 2);
     assert.equal(results[0].id, 'second');
     assert.equal(results[1].id, 'third');
@@ -46,30 +46,30 @@ describe('API Util', function() {
 
   it('filters with multiple filter groups', function() {
 
-    var data = [
+    const data = [
       {id:'first', attribute:[{value:'incorrect'}], location:{value:'london'}},
       {id:'second', attribute:[{value:'correct'}, {value:'another'}], location:{value:'new york'}},
       {id:'third', attribute:{value:'valid'}, location:{value:'paris'}}
     ];
 
-    var filters = [
+    const filters = [
       {id:'attribute', values:['correct', 'valid']},
       {id:'location', values:['new york']}
 
     ];
 
-    var results = filterData(data, filters);
+    const results = filterData(data, filters);
     assert.equal(results.length, 1);
     assert.equal(results[0].id, 'second');
   });
 
   it('doesnt filter data when no filter or filter is empty', function() {
-    var data = [
+    const data = [
       {id:'first', attribute:[{value:'incorrect'}]},
       {id:'second', attribute:[{value:'correct'}, {value:'another'}]},
       {id:'third', attribute:{value:'valid'}}
     ];
-    var results = filterData(data);
+    let results = filterData(data);
     assert.equal(results.length, 3);
 
     results = filterData(data, []);
@@ -77,17 +77,17 @@ describe('API Util', function() {
   });
 
   it('filters data when attribute not present on data', function() {
-    var data = [
+    const data = [
       {id:'first', attribute:[{value:'incorrect'}]},
       {id:'second', notattribute:[{value:'correct'}, {value:'another'}]},
       {id:'third', attribute:{value:'valid'}}
     ];
 
-    var filters = [
+    const filters = [
       {id:'attribute', values:['correct', 'valid']}
     ];
 
-    var results = filterData(data, filters);
+    const results = filterData(data, filters);
 
     assert.equal(results.length, 1);
     assert.equal(results[0].id, 'third');
