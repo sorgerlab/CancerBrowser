e9f5ae037c40905c6e51916433f69855f95ea252
Peter Beshai <peter.beshai@gmail.com>
2016-06-10 16:27:21 -0400

    Add back in wrongly deleted file, used in server code

---
+++ cancerbrowser/common/utils/fetchData.js
@@ -0,0 +1,18 @@
+export default function fetchComponentData(dispatch, components, params) {
+
+  const needs = components.reduce( (prev, current) => {
+
+    return Object.keys(current).reduce( (acc, key) => {
+      return current[key].hasOwnProperty('needs') ? current[key].needs.concat(acc) : acc;
+    }, prev);
+
+  }, []);
+
+  const promises = needs.map(need => dispatch(need(params)));
+  return Promise.all(promises);
+}
+
+export function fetchNeeds( props, needs ){
+  const { params, dispatch } = props;
+  needs.map( need => dispatch(need(params)) );
+}
